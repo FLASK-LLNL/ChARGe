af19fbd3c440bb4fa1efdebe08c231b29de40314
Shehtab <szaman5@binghamton.edu>
2025-10-02 18:35:45 -0400

    Working retroynthesis example with package

--- experiments/Retrosynthesis/RetrosynthesisExperiment.py
+++ experiments/Retrosynthesis/RetrosynthesisExperiment.py
@@ -2,34 +2,32 @@ import charge
 from charge.Experiment import Experiment
 import helper_funcs
 
+SYSTEM_PROMPT = (
+    "You are a retrosynthesis expert. Your task is to provide a retrosynthetic "
+    + "pathway for the target molecule provided by the user. The pathway should"
+    + " be provided as a tuple of reactants as SMILES and Reaction SMARTS."
+    + " Perform only single step retrosynthesis. Make sure the SMILES strings are"
+    + " valid. Make sure the reaciont SMARTS is valid. So you will generate a "
+    + "reaction SMARTS and reactants for a given molecule. "
+    + " For each reaction SMARTS verify it."
+    + " If the reaction SMARTS is valid, check if the reactants are valid SMILES."
+    + " If they are valid, check if the reaction can be performed"
+    + " Use the diagnoise tools to fix any issues that arise."
+    + " and return the reaction SMARTS, reactants, and products."
+    + " Prefer reactions that are more likely to be performed in a lab "
+    + " setting. \n\n"
+)
+
 
 class RetrosynthesisExperiment(Experiment):
     def __init__(
         self,
         user_prompt,
-        system_prompt,
     ):
         super().__init__(
-            system_prompt=system_prompt,
+            system_prompt=SYSTEM_PROMPT,
             user_prompt=user_prompt,
         )
         print("RetrosynthesisExperiment initialized with the provided prompts.")
-        self.system_prompt = system_prompt
+        self.system_prompt = SYSTEM_PROMPT
         self.user_prompt = user_prompt
-
-    @charge.hypothesis
-    def verify_reaction_SMARTS(self, smarts: str) -> bool:
-        """
-        Verify if a reaction SMARTS string is valid. Returns True if valid, False otherwise.
-
-        Args:
-            smarts (str): The input reaction SMARTS string.
-        Returns:
-            bool: True if the reaction SMARTS is valid, False otherwise.
-        raises:
-            ValueError: If the SMARTS string is invalid.
-        """
-        is_valid, message = helper_funcs.verify_reaction_SMARTS(smarts)
-        if not is_valid:
-            raise ValueError(f"Invalid reaction SMARTS: {message}")
-        return True
