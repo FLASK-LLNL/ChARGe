93cc7985f3b765d4975bfcaa6cb80257b8382670
Shehtab <szaman5@binghamton.edu>
2025-10-02 17:26:10 -0400

    Git add top level imports

--- experiments/Molecule_Generation/main.py
+++ experiments/Molecule_Generation/main.py
@@ -1,21 +1,24 @@
 import argparse
+import asyncio
 from LMOExperiment import LMOExperiment as LeadMoleculeOptimization
 import os
 
 parser = argparse.ArgumentParser()
-parser.add_argument(
-    "--lead-molecule", type=str, default="Generate a drug-like molecule"
-)
+parser.add_argument("--lead-molecule", type=str, default="CC(=O)O[C@H](C)CCN")
 parser.add_argument(
     "--client", type=str, default="autogen", choices=["autogen", "gemini"]
 )
+
 parser.add_argument(
     "--backend",
     type=str,
     default="openai",
-    choices=["openai", "gemini", "ollama", "livai", "livchat"],
+    choices=["openai", "gemini", "ollama", "livchat"],
+    help="Backend to use for the autogen client",
 )
-parser.add_argument("--model", type=str, default="gpt-4", help="Model to use")
+parser.add_argument(
+    "--model", type=str, default="gpt-4", help="Model to use for the autogen backend"
+)
 
 args = parser.parse_args()
 
@@ -35,25 +38,24 @@ if __name__ == "__main__":
 
         backend = args.backend
         model = args.model
-        if backend in ["openai", "gemini", "livai", "livchat"]:
-            kwargs = {}
-        if backend == "openai":
-            API_KEY = os.getenv("OPENAI_API_KEY")
-            model = "gpt-4"
-            kwargs["parallel_tool_calls"] = False
-            kwargs["reasoning_effort"] = "high"
-        elif backend == "livai" or backend == "livchat":
-            API_KEY = os.getenv("OPENAI_API_KEY")
-            BASE_URL = os.getenv("LIVAI_BASE_URL")
-            assert BASE_URL is not None, "LivAI Base URL must be set in environment variable"
-            model = "gpt-4.1"
-            kwargs["base_url"] = BASE_URL
-            kwargs["http_client"] = httpx.AsyncClient(verify=False)
-        else:
-            API_KEY = os.getenv("GOOGLE_API_KEY")
-            model = "gemini-flash-latest"
-            kwargs["parallel_tool_calls"] = False
-            kwargs["reasoning_effort"] = "high"
+        kwargs = {}
+        API_KEY = None
+        if backend in ["openai", "gemini", "livchat"]:
+            if backend == "openai":
+                API_KEY = os.getenv("OPENAI_API_KEY")
+                model = "gpt-4"
+                kwargs["parallel_tool_calls"] = False
+                kwargs["reasoning_effort"] = "high"
+            elif backend == "livchat":
+                API_KEY = os.getenv("OPENAI_API_KEY")
+                model = "gpt-4.1"
+                kwargs["base_url"] = "https://livai-api.llnl.gov/v1"
+                kwargs["http_client"] = httpx.AsyncClient(verify=False)
+            else:
+                API_KEY = os.getenv("GOOGLE_API_KEY")
+                model = "gemini-flash-latest"
+                kwargs["parallel_tool_calls"] = False
+                kwargs["reasoning_effort"] = "high"
 
         runner = AutoGenClient(
             experiment_type=myexperiment,
@@ -63,11 +65,10 @@ if __name__ == "__main__":
             model_kwargs=kwargs,
         )
 
-    runner.setup_mcp_servers(myexperiment)
-    results = runner.run(myexperiment)
-    print(results)
+    # results = asyncio.run(runner.run(myexperiment))
+    # print(results)
 
-    while True:
-        cont = input("Additional refinement? ...")
-        results = runner.refine(cont)
-        print(results)
+    # while True:
+    #     cont = input("Additional refinement? ...")
+    #     results = asyncio.run(runner.refine(cont))
+    #     print(results)
