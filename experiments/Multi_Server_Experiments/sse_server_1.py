43d8b440db3aa521c8197c19295184757ff2330a
Shehtab <szaman5@binghamton.edu>
2025-10-03 18:01:03 -0400

    Add multi-server example

---
+++ experiments/Multi_Server_Experiments/sse_server_1.py
@@ -0,0 +1,42 @@
+from mcp.server.fastmcp import FastMCP
+from rdkit import Chem
+
+from loguru import logger
+
+
+mcp = FastMCP("Database MCP Server that keeps track of known molecules")
+logger.info("Starting Database MCP Server")
+
+database_of_smiles = []
+
+NUM_HITS = 1
+
+
+@mcp.tool()
+def known_smiles(smiles: str) -> bool:
+    """
+    Check if a SMILES string is already known.
+    """
+
+    try:
+        global NUM_HITS
+        logger.info(f"Tool has been call: {NUM_HITS} times")
+
+        NUM_HITS += 1
+        logger.info(f"Checking if SMILES is known: {smiles}")
+        mol = Chem.MolFromSmiles(smiles)
+        smiles = Chem.MolToSmiles(mol, isomericSmiles=True)
+
+        if smiles in database_of_smiles:
+            return True
+        else:
+            database_of_smiles.append(smiles)
+            logger.info(f"SMILES not found in the database: {smiles}")
+            return False
+
+    except Exception as e:
+        return False
+
+
+if __name__ == "__main__":
+    mcp.run(transport="sse")
