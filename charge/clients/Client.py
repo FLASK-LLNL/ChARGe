ede7ca198745f0257acabd99abc85c10db26f196
Shehtab <szaman5@binghamton.edu>
2025-10-03 16:59:37 -0400

    Adding abstract step method

--- charge/clients/Client.py
+++ charge/clients/Client.py
@@ -75,6 +75,10 @@ class Client:
         raise NotImplementedError("Subclasses must implement this method.")
 
     @abstractmethod
+    async def step(self, agent, task: str):
+        raise NotImplementedError("Subclasses must implement this method.")
+
+    @abstractmethod
     async def chat(self):
         raise NotImplementedError("Subclasses must implement this method.")
 
