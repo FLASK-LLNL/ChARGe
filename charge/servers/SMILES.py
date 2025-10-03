f046290b5a203931e09a1fa34760db2f46d98cb9
Brian C. Van Essen <vanessen1@llnl.gov>
2025-10-03 14:49:29 -0700

    Refactored the server definitions into package level servers, one for manipulating SMILE strings and one for verifying reactions via SMARTS.

---
+++ charge/servers/SMILES.py
@@ -0,0 +1,112 @@
+################################################################################
+## Copyright 2025 Lawrence Livermore National Security, LLC. and Binghamton University.
+## See the top-level LICENSE file for details.
+##
+## SPDX-License-Identifier: Apache-2.0
+################################################################################
+
+from mcp.server.fastmcp import FastMCP
+try:
+    from rdkit import Chem
+    from rdkit.Chem import AllChem, Descriptors
+    from rdkit.Contrib.SA_Score import sascorer
+except ImportError:
+    raise ImportError(
+        "Please install the rdkit package to use this module."
+    )
+
+from loguru import logger
+import logging
+
+SMILES_mcp = FastMCP("[RDKit] Chem and BioInformatics MCP Server")
+
+logger.info("[RDKit] Starting Chem and BioInformatics MCP Server")
+
+logging.basicConfig(level=logging.DEBUG)
+
+@SMILES_mcp.tool()
+def canonicalize_smiles(smiles: str) -> str:
+    """
+    Canonicalize a SMILES string.
+    """
+    try:
+        logger.info(f"Canonicalizing SMILES: {smiles}")
+        mol = Chem.MolFromSmiles(smiles)
+        return Chem.MolToSmiles(mol)
+    except Exception as e:
+        return smiles
+
+
+@SMILES_mcp.tool()
+def verify_smiles(smiles: str) -> bool:
+    """
+    Verify if a SMILES string is valid.
+    """
+    try:
+        logger.info(f"Verifying SMILES: {smiles}")
+        Chem.MolFromSmiles(smiles)
+        return True
+    except Exception as e:
+        return False
+
+# @mcp.tool()
+# def verify_smiles(smiles: str) -> Tuple[bool, str]:
+#     """
+#     Verify if a SMILES string is valid.
+#     Returns a tuple of (bool, str).
+#     The bool indicates if the SMILES is valid, and the str is an error message if it is not.
+#     """
+#     try:
+#         logger.info(f"Verifying SMILES: {smiles}")
+#         Chem.MolFromSmiles(smiles)
+#         return True, "SMILES is valid."
+#     except Exception as e:
+#         return False, f"Invalid SMILES string: {e}"
+
+
+@SMILES_mcp.tool()
+def get_synthesizability(smiles: str) -> float:
+    """
+    Calculate the synthesizability of a molecule given its SMILES string.
+    Values rankge from 1.0 (highly synthesizable) to 10.0 (not synthesizable).
+    """
+
+    try:
+        # logger.info(f"Calculating synthesizability for SMILES: {smiles}")
+        mol = Chem.MolFromSmiles(smiles)
+        if mol is None:
+            logger.warning("Invalid SMILES string or molecule could not be created.")
+            return 10.0  # Default value for invalid SMILES
+        score = sascorer.calculateScore(mol)
+        logger.info(f"Synthesizability score for SMILES {smiles}: {score}")
+        return score
+    except Exception as e:
+        logger.error(f"Error creating molecule from SMILES: {e}")
+        return 10.0
+
+
+database_of_smiles = []
+
+
+@SMILES_mcp.tool()
+def known_smiles(smiles: str) -> bool:
+    """
+    Check if a SMILES string is already known.
+    """
+
+    try:
+        logger.info(f"Checking if SMILES is known: {smiles}")
+        mol = Chem.MolFromSmiles(smiles)
+        smiles = Chem.MolToSmiles(mol, isomericSmiles=True)
+
+        if smiles in database_of_smiles:
+            return True
+        else:
+            database_of_smiles.append(smiles)
+            logger.info(f"SMILES not found in the database: {smiles}")
+            return False
+
+    except Exception as e:
+        return False
+
+
