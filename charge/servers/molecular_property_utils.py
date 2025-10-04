e71f5d649a1d0f7403c5a77d35065951b43935b7
Brian C. Van Essen <vanessen1@llnl.gov>
2025-10-03 20:38:38 -0700

    Added further description to the combined calculate density and synthesizability function.

--- charge/servers/molecular_property_utils.py
+++ charge/servers/molecular_property_utils.py
@@ -58,6 +58,10 @@ def get_density_and_synthesizability(smiles: str) -> tuple[float, float]:
     Calculate the density and synthesizability of a molecule given its SMILES string.
     Returns a tuple of (density, synthesizability).
 
+    Density is the molecular weight of the molecule per unit volume.
+    In units of unified atomic mass (u) per cubic Angstroms (A^3)
+    Synthesizable values range from 1.0 (highly synthesizable) to 10.0 (not synthesizable).
+
     Args:
         smiles (str): The input SMILES string.
     Returns:
